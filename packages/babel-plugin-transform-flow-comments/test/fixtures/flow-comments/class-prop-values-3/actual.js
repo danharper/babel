3423b75dc8ada2f9df60da9773884cf1b39a4be9
Dan Harper <intouch@danharper.me>
2016-10-01 00:44:37 +0100

    add to transform test too

--- packages/babel-plugin-transform-flow-comments/test/fixtures/flow-comments/class-prop-values-3/actual.js
+++ packages/babel-plugin-transform-flow-comments/test/fixtures/flow-comments/class-prop-values-3/actual.js
@@ -2,6 +2,8 @@ class X {
   a: number = 2
   b: ?string = '3'
   c: ?number
+  v: (a/* x = y */: string) => /* blah = blah */ void
+  w: (a/* x = y */: string) => /* blah = blah */ number = (a/* x = y */: string) => /* blah = blah */ 2
   x: (a: string) => string
   y: (a: string) => string = (a: string) => '2'
   z: (a: string) => (b: number) => { x: string, y: number } = (a: string) => (b: number) => ({ x: a, y: b })
--- packages/babel-plugin-transform-flow-comments/test/fixtures/flow-comments/class-prop-values-3/expected.js
+++ packages/babel-plugin-transform-flow-comments/test/fixtures/flow-comments/class-prop-values-3/expected.js
@@ -3,11 +3,14 @@ class X {
     this.a = 2;
     this.b = '3';
 
+    this.w = (a /* blah = blah */ /* x = y */ /*: string*/) => 2;
+
     this.y = (a /*: string*/) => '2';
 
     this.z = (a /*: string*/) => (b /*: number*/) => ({ x: a, y: b });
   } /*:: a: number*/
 
   /*:: b: ?string*/ /*:: c: ?number*/
-  /*:: z: (a: string) => (b: number) => { x: string, y: number }*/ /*:: x: (a: string) => string*/ /*:: y: (a: string) => string*/
+  /*:: v: (a/* x = y *-/: string) => /* blah = blah *-/ void*/
+  /*:: w: (a/* x = y *-/: string) => /* blah = blah *-/ number*/ /*:: z: (a: string) => (b: number) => { x: string, y: number }*/ /*:: x: (a: string) => string*/ /*:: y: (a: string) => string*/
 }
